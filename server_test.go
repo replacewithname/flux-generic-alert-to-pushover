e7ef15327a537ec2a8b0ae1cb455f1c10f4ec76d
Christopher Gertz <mail@christophergertz.de>
2022-04-11 17:26:36 +0200

    Mock Pushover API in test

--- server_test.go
+++ server_test.go
@@ -1,16 +1,20 @@
 package main
 
 import (
+	"flux-generic-alert-to-pushover/config"
 	"flux-generic-alert-to-pushover/handler"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
+	"github.com/gregdel/pushover"
 	"github.com/labstack/echo/v4"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestHappyPath(t *testing.T) {
+	conf := &config.Config{PushoverApiKey: "a34wsc2e7xdwbbwntgewqybys4fmn9", PushoverUserKey: "uff7qpdvsa9czkjetx4m7i7c9ff689"}
+
 	// Setup
 	e := echo.New()
 	req := httptest.NewRequest(http.MethodPost, "/send-generic-to-pushover", nil)
@@ -18,7 +22,18 @@ func TestHappyPath(t *testing.T) {
 	rec := httptest.NewRecorder()
 	context := e.NewContext(req, rec)
 
-	h := &handler.Handler{}
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, _ *http.Request) {
+		// For some reason, this is the only thing which is checked by the framework
+		w.Header().Add("x-limit-app-limit", "10000")
+		w.Header().Add("x-limit-app-remaining", "9994")
+		w.Header().Add("x-limit-app-reset", "1651381200")
+
+		w.Write([]byte(`{"status":1,"request":"bf59e63c-8519-401e-ad74-74b777bf9b85"}`))
+	}))
+
+	pushover.APIEndpoint = server.URL
+
+	h := &handler.Handler{Config: conf}
 
 	if assert.NoError(t, h.SendGenericToPushover(context)) {
 		assert.Equal(t, http.StatusOK, rec.Code)
